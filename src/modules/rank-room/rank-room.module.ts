2239cec7584089318c5c697801cee2b0c6375c4f
Dai Phung Van <pvd@aod.vn>
2026-04-07 16:50:48 +0700

    fix room

--- src/modules/rank-room/rank-room.module.ts
+++ src/modules/rank-room/rank-room.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { RankRoomService } from './rank-room.service';
-import { RankRoomController } from './rank-room.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RankRoom } from 'src/model';
+import { RankRoomController } from './rank-room.controller';
+import { RankRoomService } from './rank-room.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([RankRoom])],
--- src/modules/room/room.module.ts
+++ src/modules/room/room.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { RoomService } from './room.service';
 import { RoomController } from './room.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Room } from 'src/model';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Room])],
   controllers: [RoomController],
   providers: [RoomService],
 })
 export class RoomModule {}
+
