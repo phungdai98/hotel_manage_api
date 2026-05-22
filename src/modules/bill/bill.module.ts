07126fd5d360332e17ec9e44ade6ddb0635c3d72
Dai Phung Van <pvd@aod.vn>
2026-05-22 17:03:38 +0700

    fix import provider

--- src/modules/bill/bill.module.ts
+++ src/modules/bill/bill.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
-import { BillService } from './bill.service';
-import { BillController } from './bill.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Bill, RentTicket } from 'src/model';
+import { Bill } from 'src/model';
+import { RentModule } from '../rent/rent.module';
+import { BillController } from './bill.controller';
+import { BillService } from './bill.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Bill, RentTicket])],
+  imports: [TypeOrmModule.forFeature([Bill]), RentModule],
   controllers: [BillController],
   providers: [BillService],
 })
-export class BillModule {}
+export class BillModule { }
